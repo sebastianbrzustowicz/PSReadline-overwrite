616a70cdf3a6b8b43f049e671fc1c7e8115c4d23
sebastianbrzustowicz <Se.Brzustowicz@gmail.com>
2024-06-19 22:38:11 +0200

    adding script

---
+++ PSReadline-overwrite.ps1
@@ -0,0 +1,33 @@
+function Get-RandomCommand {
+    $commands = @(
+        "Get-Process",
+        "Get-Service",
+        "Get-EventLog -LogName Application",
+        "Get-ChildItem",
+        "Set-Location",
+        "New-Item -ItemType File",
+        "Remove-Item",
+        "Copy-Item",
+        "Move-Item",
+        "Start-Process",
+        "Stop-Process",
+        "Get-Content",
+        "Set-Content",
+        "Add-Content",
+        "Get-Help",
+        "Get-Command",
+        "Get-Module",
+        "Import-Module",
+        "Export-ModuleMember",
+        "Get-Alias"
+    )
+    return $commands | Get-Random
+}
+
+$randomCommands = @()
+
+1..4096 | ForEach-Object {
+    $randomCommands += Get-RandomCommand
+}
+
+$randomCommands | Out-File -FilePath "$env:USERPROFILE\AppData\Roaming\Microsoft\Windows\PowerShell\PSReadline\ConsoleHost_history.txt"
